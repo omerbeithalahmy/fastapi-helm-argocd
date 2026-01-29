fe82d691fdb9ba1e9698dcdc557e727b7a0c9746
omerbeithalahmy <omer24883@gmail.com>
2026-01-29 14:53:05 +0200

    Add CI with tests and updated structure

---
+++ app/test_main.py
@@ -0,0 +1,13 @@
+from fastapi.testclient import TestClient
+from app import app
+
+client = TestClient(app)
+
+def test_read_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello from FastAPI Helm ArgoCD demo!"}
+
+def test_health_check():
+    response = client.get("/")
+    assert response.status_code == 200
